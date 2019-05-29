533354dfa97d4c3fdc42fbbdf3d23d70e480e2bf
Ernesto <ernestoalejandro738@gmail.com>
2019-05-29 16:18:33 -0400

    add user model

---
+++ src/app/models/user.ts
@@ -0,0 +1,11 @@
+export class FirebaseUserModel {
+  image: String;
+  displayName: String;
+  uid: String;
+
+  constructor(user: firebase.User) {
+    this.image = user.photoURL;
+    this.displayName = user.displayName;
+    this.uid = user.uid;
+  }
+}
