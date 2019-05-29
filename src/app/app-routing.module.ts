1332f7831607a37ca3c7489271a7580abc8d2caa
Ernesto <ernestoalejandro738@gmail.com>
2019-05-29 16:20:23 -0400

    add admin route with guard and login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './services/auth-guard.service';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [{
-  path: 'admin',
-  loadChildren: './modules/admin/admin.module#AdminModule'
+  path: '',
+  children: [
+    {
+      path: 'admin',
+      loadChildren: './modules/admin/admin.module#AdminModule',
+      canActivate: [AuthGuard]
+    }, {
+      path: 'login',
+      component: LoginComponent
+    }]
 }];
 
 @NgModule({
