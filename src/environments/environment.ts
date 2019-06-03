293ce0d1db30f97c767982ec714dcb3f8b5e9317
Ernesto Alejandro Santana <ernesto@tropixme.com>
2019-06-03 12:31:16 -0400

    add firebase_config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   FIREBASE_CONFIG: {
-    apiKey: "AIzaSyAWz86sv3FubhM8n2Gql3iT66jgzhiDZBk",
-    authDomain: "fir-test-31a92.firebaseapp.com",
-    databaseURL: "https://fir-test-31a92.firebaseio.com",
-    projectId: "fir-test-31a92",
-    storageBucket: "fir-test-31a92.appspot.com",
-    messagingSenderId: "819726840338",
-    appId: "1:819726840338:web:3f532eff160d6d1e"
+    apiKey: 'AIzaSyAWz86sv3FubhM8n2Gql3iT66jgzhiDZBk',
+    authDomain: 'fir-test-31a92.firebaseapp.com',
+    databaseURL: 'https://fir-test-31a92.firebaseio.com',
+    projectId: 'fir-test-31a92',
+    storageBucket: 'fir-test-31a92.appspot.com',
+    messagingSenderId: '819726840338',
+    appId: '1:819726840338:web:3f532eff160d6d1e'
   }
 };
 
